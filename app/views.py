1f42494293828f535c775a6379c1e90a20cc5505
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-12 18:30:43 +0300

    Add the entire news list

--- app/views.py
+++ app/views.py
@@ -13,12 +13,12 @@ def index():
     '''
     #Getting general news
     general_news = get_news('general')
-    # business_news = get_news('business')
-    # sports_news = get_news('sports')
-    print(general_news)
+    business_news = get_news('business')
+    sports_news = get_news('sports')
+   # print(general_news)
     title = 'Home - Welcome To The Best News Website Online'
-    return render_template('index.html', title = title, general = general_news) 
+    return render_template('index.html', title = title, general = general_news, business = business_news, sports = sports_news ) 
 
-    #business = business_news, sports = sports_news 
+    
     
     
