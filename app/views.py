684b30a44d308884753b0ff38c5108b42b115c21
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-12 14:49:33 +0300

    Add the initial site with title

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from flask import render_template
 from app import app
 
+#movies is news
+
 # Views
-@app.route('/')
-def index():
+@app.route('/news/<int:news_id>')
+def news():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Home - Welcome To The Best News Website Online'
+    return render_template('index.html',title = title)
