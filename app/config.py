604df23b493a69a5e70780412ca66fde48c4d411
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-12 18:14:41 +0300

    Add the news list

--- app/config.py
+++ app/config.py
@@ -2,6 +2,25 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
 
-=db80159a19ff4fceac0d1b09a7288d5a
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True    
+
--- app/views.py
+++ app/views.py
@@ -1,14 +1,24 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 #movies is news
 
 # Views
-@app.route('/news/<int:news_id>')
-def news():
+@app.route('/')
+def index():
 
     '''
     View root page function that returns the index page and its data
     '''
+    #Getting general news
+    general_news = get_news('general')
+    # business_news = get_news('business')
+    # sports_news = get_news('sports')
+    print(general_news)
     title = 'Home - Welcome To The Best News Website Online'
-    return render_template('index.html',title = title)
+    return render_template('index.html', title = title, general = general_news) 
+
+    #business = business_news, sports = sports_news 
+    
+    
