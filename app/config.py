c82074b528177bc0c8935936bbf34325ae3d8877
Ephraim-Kamau <kephraim19@gmail.com>
2019-04-12 15:36:18 +0300

    Add the working json response

--- app/config.py
+++ app/config.py
@@ -2,5 +2,6 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?country=us/{}?api_key={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
 
+=db80159a19ff4fceac0d1b09a7288d5a
